60d6a1493cc7506756615f1772cb9b71caa236c8
veronica borges <veronica.bores@alunoescolassociais.org.br>
2023-10-27 08:52:37 -0300

    Começando a documentar o README

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # E-commerce-
+
+# Índice
+
+[Cadastro de Endereço]()  
+[Introdução]()  
+[Descrição]()  
+[Tecnologias ultilizadas ]()  
+[Funcionalidade]()  
+[Fontes consultadas ]()   
+[Autores]()  
+
+# E-commerce 🔗
+
+
+![Capa do projeto]()
+
+## Introdução 
+
+
+## Descrição
+
+
+## Tecnologias Utilizadas
+
+A seguir, listamos as principais tecnologias utilizadas neste projeto:
+
+| Tecnologia         | Descrição             |
+|--------------------|-----------------------|
+| VS Code (Visual Code) | Editor de código-fonte  |
+| GitHub             | Plataforma de hospedagem de código |
+| HTML               | Linguagem de marcação  |
+| CSS                | Folhas de estilo em cascata |
+| JSS                | Lib de estilo em JavaScript |
+
+
+## Funcionalidade 
+
+
+## Fontes consultadas 
+
+## Autores 
+Veronica Borges
--- login.js
+++ login.js
@@ -1,70 +1,53 @@
+"use strict";
+
+// Função para limpar o formulário
 const limparFormulario = () => {
-  document.getElementById('rua').value = '';
-  document.getElementById('bairro').value = '';
-  document.getElementById('cidade').value = '';
-  document.getElementById('estado').value = '';
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
 }
 
-const mostrarMensagem = (mensagem, sucesso = true) => {
-  const mensagemElement = document.getElementById('mensagem');
-  mensagemElement.innerText = mensagem;
-  mensagemElement.style.color = sucesso ? 'green' : 'red';
-}
+// Função para verificar se é um número
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
 
+// Função para verificar se o CEP é válido
+const cepValido = (cep) => cep.length === 8 && eNumero(cep);
+
+// Função para preencher o formulário com os dados do endereço
 const preencherFormulario = (endereco) => {
-  document.getElementById('rua').value = endereco.logradouro;
-  document.getElementById('bairro').value = endereco.bairro;
-  document.getElementById('cidade').value = endereco.localidade;
-  document.getElementById('estado').value = endereco.uf;
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
 }
 
+// Função para fazer a pesquisa do CEP na API da Via CEP
 const pesquisarCep = async () => {
-  limparFormulario();
-  mostrarMensagem('Pesquisando CEP...', true);
-  const cep = document.getElementById('cep').value;
+    limparFormulario();
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
 
-  // Fazer a consulta à API Via CEP
-  const url = `https://viacep.com.br/ws/${cep}/json/`;
-  
-  if (cep.length === 8) {
-    try {
-      const response = await fetch(url);
-
-      if (response.ok) {
-        const address = await response.json();
-
-        if (!address.erro) {
-          preencherFormulario(address);
-          mostrarMensagem('CEP encontrado com sucesso!', true);
-        } else {
-          mostrarMensagem('CEP não encontrado. Verifique o CEP digitado.', false);
+    if (cepValido(cep)) {
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const endereco = await response.json();
+                if (!endereco.hasOwnProperty('erro')) {
+                    preencherFormulario(endereco);
+                } else {
+                    alert('CEP não encontrado');
+                }
+            } else {
+                alert('Erro ao buscar o CEP');
+            }
+        } catch (error) {
+            alert('Ocorreu um erro na pesquisa de CEP');
         }
-      } else {
-        mostrarMensagem('Erro ao buscar o CEP. Tente novamente mais tarde.', false);
-      }
-    } catch (error) {
-      console.error('Erro na solicitação:', error);
-      mostrarMensagem('Ocorreu um erro na solicitação. Verifique sua conexão ou tente novamente mais tarde.', false);
+    } else {
+        alert('CEP incorreto');
     }
-  } else {
-    mostrarMensagem('CEP incorreto. Certifique-se de que o CEP possui 8 dígitos numéricos.', false);
-  }
 }
 
+// Adiciona um evento de foco no input do CEP para acionar a pesquisa
 document.getElementById('cep').addEventListener('blur', pesquisarCep);
-
-// Adicione este bloco para ativar o preenchimento automático quando o CEP é digitado
-document.getElementById('cep').addEventListener('input', (event) => {
-  const cep = event.target.value;
-
-  if (cep.length === 8) {
-    pesquisarCep();
-  }
-});
-
-document.getElementById('cep').addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault(); // Impede o comportamento padrão
-    pesquisarCep();
-  }
-});
